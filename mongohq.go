6047a9d14a37e1dcf8998e221d4e444989292507
David Seelig <seeligd@gmail.com>
2015-02-15 16:05:45 -0800

    search

--- mongohq.go
+++ mongohq.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"html/template"
 	"net/http"
-	//"os"
 	"time"
 )
 
 type Data struct {
+	Person Person
+	Time   time.Time
+}
+type Person struct {
 	Name  string
 	Email string
-	Time  time.Time
 }
 
 func main() {
 	http.HandleFunc("/", root)
-	http.HandleFunc("/d", display)
+	http.HandleFunc("/display", display)
 	err := http.ListenAndServe(":"+getPort(), nil)
 	if err != nil {
 		panic(err)
@@ -29,12 +33,40 @@ func main() {
 // will return even if no match is made
 func root(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("default.html", "header.html", "footer.html")
-	p := Data{"Boris", "boris@yeltsin.gov", time.Now()}
+	p := Data{Person{"Boris", ""}, time.Now()}
 	t.Execute(w, p)
 }
 
+/*
+func insertValues() {
+}
+*/
+
 func display(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "hellow")
+	displayTemplate, _ := template.ParseFiles("display.html", "header.html", "footer.html")
+	session, err := mgo.Dial("localhost")
+
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+	session.SetSafe(&mgo.Safe{})
+
+	collection := session.DB("test").C("People")
+	result := Person{}
+	err = collection.Find(bson.M{"name": r.FormValue("name")}).One(&result)
+
+	fmt.Println(err)
+
+	data := Data{result, time.Now()}
+	if result.Email != "" {
+		errn := displayTemplate.Execute(w, data)
+		if errn != nil {
+			http.Error(w, errn.Error(), http.StatusInternalServerError)
+		}
+	} else {
+		displayTemplate.Execute(w, data)
+	}
 }
 
 func getPort() string {
